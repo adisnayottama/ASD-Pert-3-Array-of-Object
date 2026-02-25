0c22928ffda9b0b6976c1cb5a4971fa24e39fec6
adisnayottama <adisnayottama1@gmail.com>
2026-02-25 11:51:30 -1200

    matkulFix

--- Praktikum03/Matakuliah02.java
+++ Praktikum03/Matakuliah02.java
@@ -6,10 +6,28 @@ public class Matakuliah02 {
     public int sks;
     public int jumlahJam;
 
-    public Matakuliah02 (String kode, String nama, int sks, int jumlahJam) {
+    public Matakuliah02() {
+    }
+
+    public Matakuliah02(String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
+    }
+
+    public void tambahData(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+
+    public void cetakInfo() {
+        System.out.println("Kode        : " + kode);
+        System.out.println("Nama        : " + nama);
+        System.out.println("SKS         : " + sks);
+        System.out.println("Jumlah Jam  : " + jumlahJam);
+        System.out.println("------------------------------------");
     }
 }
