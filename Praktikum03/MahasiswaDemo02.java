7cbd881a4d0acd95624f6adad02869e624345ad1
adisnayottama <adisnayottama1@gmail.com>
2026-02-25 11:03:28 -1200

    modifMainPerc2

--- Praktikum03/MahasiswaDemo02.java
+++ Praktikum03/MahasiswaDemo02.java
@@ -25,11 +25,7 @@ public class MahasiswaDemo02 {
 
     for (int i = 0; i < 3; i++) {
         System.out.println("Data Mahasiswa ke-" + (i + 1));
-        System.out.println("NIM     : "+ arrayOfMahasiswa02[i].nim);
-        System.out.println("Nama    : "+ arrayOfMahasiswa02[i].nama);
-        System.out.println("Kelas   : "+ arrayOfMahasiswa02[i].kelas);
-        System.out.println("IPK     : "+ arrayOfMahasiswa02[i].ipk);
-        System.out.println("--------------------------------");
+        arrayOfMahasiswa02[i].cetakInfo();
     }
 
     }
